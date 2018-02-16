5783a7e3d5107ad3c7a6ea87ad4b94de51fe5cab
saito-yusuke-gb <saito-yusuke-gb@ynu.jp>
2018-02-16 19:44:57 +0900

    とりあえずpythonファイルと同じディレクトリにcsvファイル置くver.

--- sanuiGraphGenerator/sanuiGraphGenerator.py
+++ sanuiGraphGenerator/sanuiGraphGenerator.py
@@ -17,11 +17,21 @@ class InputWindow(QWidget):
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
             event.accept()
+            
+            for mimetype in event.mimeData().formats():
+                print('MIMEType: ', mimetype)
+                print('Data: ', event.mimeData().data(mimetype))
+                print()
+            print()
         else:
             event.ignore()
 
-    def dropEvent(self, QDropEvent):
-        return super().dropEvent(QDropEvent)
+    def dropEvent(self, event):
+        event.accept()
+        mimeData = event.mimeData()
+        for mimetype in event.mimeData().formats():
+            print('MIMEType: ', mimetype)
+            print('Data: ', event.mimeData().data(mimetype))
 
     def initUI(self):
 
@@ -84,9 +94,51 @@ class InputWindow(QWidget):
     def outPutButtonClicked(self):
         sender = self.sender()
 
+        df = pd.read_csv('sample.csv').dropna()
+        header = df.columns.values.tolist()
+        data = df.values
+
+        xi = df['x'].T
+        yi = df['y1'].T + df.['y2'] + df.['y3'].T
+        sum_xx = sum(xi ** 2)
+        sum_xy = sum(xi * yi)
+        slope = sum_xy / sum_xx
+        sigma = np.std(yi - slope * xi)
+
         fig = plt.figure()
+        ax = fig.add_subplot(111)
+        left = np.array(df['x'].T)
+        height_y1 = np.array(df['y1'].T)
+        height_y2 = np.array(df['y2'].T)
+        height_y3 = np.array(df['y3'].T)
+        regLine = lin.Line2D([0, 12], [0, slope * 12], linewidth = 3, color = "black")
+        regLine_sigma = lin.Line2D([0, 12], [sigma, slope * 12 + sigma], color = "black")
+        regLine_2sigma = lin.Line2D([0, 12], [2 * sigma, slope * 12 + 2 * sigma], color = "black")
+        regLine_minus_sigma = lin.Line2D([0, 12], [-1 * sigma, slope * 12 - sigma], color = "black")
+        regLine_minus_2sigma = lin.Line2D([0, 12], [-2 * sigma, slope * 12 - 2 * sigma], color = "black")
+        bar_y1 = plt.bar(left, height_y1, color = '#006400', width = 0.05)
+        bar_y2 = plt.bar(left, height_y2, bottom = height_y1, color = '#ffd700', width = 0.05)
+        bar_y3 = plt.bar(left, height_y3, bottom = height_y1 + height_y2, color = '#4169e1', width = 0.05)
 
+        if self.regLine1RadioButton.isChecked():
+            pass
+        elif self.regLine2RadioButton.isChecked():
+            ax.add_line(lin.Line2D([0, 12], [0, 12], linewidth = 3, color = "black"))
+        elif self.regLine3RadioButton.isChecked():
+            ax.add_line(regLine)
+            ax.add_line(regLine_sigma)
+            ax.add_line(regLine_2sigma)
+            ax.add_line(regLine_minus_sigma)
+            ax.add_line(regLine_minus_2sigma)
 
+        plt.xlim([0, 10])
+        plt.ylim([0, 10])
+        plt.title("ECOLOG Estimation", fontsize = 20)
+        plt.xlabel("Energy Consumption from CAN [kWh]", fontsize = 16)
+        plt.ylabel("Estimated Energy Consumption [kWh]", fontsize = 16)
+        ax.set_aspect('equal')
+        plt.legend((bar_y1[0], bar_y2[0], bar_y3[0]), ("ECOLOG Estimation", "AUX Consumption", "Air Conditioner Estimation"), fontsize = 16)
+        plt.show()
 
 if __name__== "__main__":
     app = QApplication(sys.argv)
