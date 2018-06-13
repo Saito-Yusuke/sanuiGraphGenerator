1a42ad3cee50876194daa292b932f06e150c23bc
saito-yusuke-gb <saito-yusuke-gb@ynu.jp>
2018-06-13 14:03:20 +0900

    線形回帰に対応

--- sanuiGraphGenerator/sanuiGraphGenerator.py
+++ sanuiGraphGenerator/sanuiGraphGenerator.py
@@ -14,24 +14,17 @@ class InputWindow(QWidget):
         self.setAcceptDrops(True)
         self.initUI()
 
+    #dragの境界侵入時
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
             event.accept()
-            
-            for mimetype in event.mimeData().formats():
-                print('MIMEType: ', mimetype)
-                print('Data: ', event.mimeData().data(mimetype))
-                print()
-            print()
         else:
             event.ignore()
 
+    #dropしたとき
     def dropEvent(self, event):
         event.accept()
         mimeData = event.mimeData()
-        for mimetype in event.mimeData().formats():
-            print('MIMEType: ', mimetype)
-            print('Data: ', event.mimeData().data(mimetype))
 
     def initUI(self):
 
@@ -52,7 +45,8 @@ class InputWindow(QWidget):
         # RadioButtonオブジェクト
         self.regLine1RadioButton = QRadioButton("なし")
         self.regLine2RadioButton = QRadioButton("y = x")
-        self.regLine3RadioButton = QRadioButton("y = ax (a: 相関)")
+        self.regLine3RadioButton = QRadioButton("y = ax")
+        self.regLine4RadioButton = QRadioButton("y = ax + b")
         self.regLine1RadioButton.setChecked(True)
 
         # Buttonオブジェクト
@@ -66,6 +60,7 @@ class InputWindow(QWidget):
         regLineBox.addWidget(self.regLine1RadioButton)
         regLineBox.addWidget(self.regLine2RadioButton)
         regLineBox.addWidget(self.regLine3RadioButton)
+        regLineBox.addWidget(self.regLine4RadioButton)
         self.regLineGroupBox.setLayout(regLineBox)
 
         # How to Use関連のwidgetグループ
@@ -100,10 +95,27 @@ class InputWindow(QWidget):
 
         xi = df['x'].T
         yi = df['y1'].T + df['y2'] + df['y3'].T
+
+        n = len(data)
+        print("n = " + str(n))
+        sum_x = sum(xi)
+        sum_y = sum(yi)
         sum_xx = sum(xi ** 2)
         sum_xy = sum(xi * yi)
-        slope = sum_xy / sum_xx
-        sigma = np.std(yi - slope * xi)
+
+        if self.regLine2RadioButton.isChecked():
+            slope = 1
+            intercept = 0
+        elif self.regLine3RadioButton.isChecked():
+            slope = sum_xy / sum_xx
+            intercept = 0
+        elif self.regLine4RadioButton.isChecked():
+            #最小二乗法による係数決定
+            slope = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x ** 2)
+            intercept = (sum_xx * sum_y - sum_xy * sum_x) / (n * sum_xx - sum_x ** 2)
+
+        #回帰直線との標準偏差
+        sigma = np.std(yi - slope * xi - intercept)
 
         fig = plt.figure()
         ax = fig.add_subplot(111)
@@ -111,26 +123,25 @@ class InputWindow(QWidget):
         height_y1 = np.array(df['y1'].T)
         height_y2 = np.array(df['y2'].T)
         height_y3 = np.array(df['y3'].T)
-        regLine = lin.Line2D([0, 12], [0, slope * 12], linewidth = 3, color = "black")
-        regLine_sigma = lin.Line2D([0, 12], [sigma, slope * 12 + sigma], color = "black")
-        regLine_2sigma = lin.Line2D([0, 12], [2 * sigma, slope * 12 + 2 * sigma], color = "black")
-        regLine_minus_sigma = lin.Line2D([0, 12], [-1 * sigma, slope * 12 - sigma], color = "black")
-        regLine_minus_2sigma = lin.Line2D([0, 12], [-2 * sigma, slope * 12 - 2 * sigma], color = "black")
+        regLine = lin.Line2D([0, 12], [intercept, slope * 12 + intercept], linewidth = 3, color = "black")
+        regLine_sigma = lin.Line2D([0, 12], [intercept + sigma, slope * 12 + intercept + sigma], color = "black")
+        regLine_2sigma = lin.Line2D([0, 12], [intercept + 2 * sigma, slope * 12 + intercept + 2 * sigma], color = "black")
+        regLine_minus_sigma = lin.Line2D([0, 12], [intercept - sigma, slope * 12 + intercept - sigma], color = "black")
+        regLine_minus_2sigma = lin.Line2D([0, 12], [intercept - 2 * sigma, slope * 12 + intercept - 2 * sigma], color = "black")
         bar_y1 = plt.bar(left, height_y1, color = '#006400', width = 0.05)
         bar_y2 = plt.bar(left, height_y2, bottom = height_y1, color = '#ffd700', width = 0.05)
         bar_y3 = plt.bar(left, height_y3, bottom = height_y1 + height_y2, color = '#4169e1', width = 0.05)
 
         if self.regLine1RadioButton.isChecked():
             pass
-        elif self.regLine2RadioButton.isChecked():
-            ax.add_line(lin.Line2D([0, 12], [0, 12], linewidth = 3, color = "black"))
-        elif self.regLine3RadioButton.isChecked():
+        else:
             ax.add_line(regLine)
             ax.add_line(regLine_sigma)
             ax.add_line(regLine_2sigma)
             ax.add_line(regLine_minus_sigma)
             ax.add_line(regLine_minus_2sigma)
             print("回帰直線の傾き = " + str(round(slope, 3)))
+            print("回帰直線のy切片 = " + str(round(intercept, 3)))
             print("回帰直線との標準偏差 = " + str(round(sigma, 3)))
 
         plt.xlim([0, 10])
